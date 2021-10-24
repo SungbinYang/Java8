b09394219b9e725972f1d71aa41264d2b9b9970a
Robert.Yang <ysb05222@naver.com>
2021-10-24 18:55:04 +0900

    메소드 레퍼런스
    메소드 레퍼런스

---
+++ java8/src/main/java/me/sungbin/java8to17/Greeting.java
@@ -0,0 +1,25 @@
+package me.sungbin.java8to17;
+
+public class Greeting {
+
+    private String name;
+
+    public Greeting() {
+    }
+
+    public Greeting(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String hello(String name) {
+        return "hello " + name;
+    }
+
+    public static String hi(String name) {
+        return "hi " + name;
+    }
+}
