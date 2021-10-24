93e47009f0111e0c9c8404d3dcd3f87562e8fa6e
Robert.Yang <ysb05222@naver.com>
2021-10-25 05:51:38 +0900

    자바 8 API의 기본 메소드와 스태틱 메소드
    자바 8 API의 기본 메소드와 스태틱 메소드

--- java8/src/main/java/me/sungbin/java8to17/App.java
+++ java8/src/main/java/me/sungbin/java8to17/App.java
@@ -1,5 +1,11 @@
 package me.sungbin.java8to17;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.stream.Collectors;
+
 public class App {
     public static void main(String[] args) {
 //        Greeting greeting = new Greeting();
@@ -20,10 +26,33 @@ public class App {
 //        Arrays.sort(names, String::compareToIgnoreCase);
 //        System.out.println(Arrays.toString(names));
 
-        GreetingInterface greetingInterface = new DefaultFoo("sungbin");
-        greetingInterface.printName();
-        greetingInterface.printNameUpperCase();
+//        GreetingInterface greetingInterface = new DefaultFoo("sungbin");
+//        greetingInterface.printName();
+//        greetingInterface.printNameUpperCase();
+//
+//        GreetingInterface.printAnything();
 
-        GreetingInterface.printAnything();
+        List<String> name = new ArrayList<>();
+        name.add("sungbin");
+        name.add("robert");
+        name.add("toby");
+        name.add("foo");
+
+        name.forEach(System.out::println);
+
+        Spliterator<String> spliterator = name.spliterator();
+        Spliterator<String> stringSpliterator = spliterator.trySplit();
+        while (spliterator.tryAdvance(System.out::println));
+        System.out.println("============================");
+        while (stringSpliterator.tryAdvance(System.out::println));
+
+        System.out.println(name.stream().map(String::toUpperCase).filter(s -> s.startsWith("S")).collect(Collectors.toList()));
+
+//        name.removeIf(s -> s.startsWith("s"));
+//        name.forEach(System.out::println);
+
+        Comparator<String> compareToIgnoreCase = String::compareToIgnoreCase;
+        name.sort(compareToIgnoreCase.reversed());
+        name.forEach(System.out::println);
     }
 }
