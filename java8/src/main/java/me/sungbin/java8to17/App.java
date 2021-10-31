ef27f225c3fd18e9227b15f1931a62928a4c1d03
Robert.Yang <ysb05222@naver.com>
2021-10-31 12:38:04 +0900

    Stream API
    Stream API

--- java8/src/main/java/me/sungbin/java8to17/App.java
+++ java8/src/main/java/me/sungbin/java8to17/App.java
@@ -1,7 +1,9 @@
 package me.sungbin.java8to17;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -33,11 +35,11 @@ public class App {
 //
 //        GreetingInterface.printAnything();
 
-        List<String> names = new ArrayList<>();
-        names.add("sungbin");
-        names.add("whiteship");
-        names.add("toby");
-        names.add("foo");
+//        List<String> names = new ArrayList<>();
+//        names.add("sungbin");
+//        names.add("whiteship");
+//        names.add("toby");
+//        names.add("foo");
 
 //        name.forEach(System.out::println);
 //
@@ -78,10 +80,54 @@ public class App {
 //        }
 
         // 병렬처리가 다 좋은것은 아니다. 보통 데이터가 엄청 많을 경우에 사용한다.
-        List<String> collect = names.parallelStream().map(s -> {
-            System.out.println(s + " " + Thread.currentThread().getName());
-            return s.toUpperCase();
-        }).collect(Collectors.toList());
-        collect.forEach(System.out::println);
+        // 직접 성능측정을 해보면서 병럴처릴 할지 안 할지를 결정한다.
+//        List<String> collect = names.parallelStream().map(s -> {
+//            System.out.println(s + " " + Thread.currentThread().getName());
+//            return s.toUpperCase();
+//        }).collect(Collectors.toList());
+//        collect.forEach(System.out::println);
+
+        List<OnlineClass> springClasses = new ArrayList<>();
+        springClasses.add(new OnlineClass(1, "spring boot", true));
+        springClasses.add(new OnlineClass(2, "spring data jpq", true));
+        springClasses.add(new OnlineClass(3, "spring mvc", false));
+        springClasses.add(new OnlineClass(4, "spring core", false));
+        springClasses.add(new OnlineClass(5, "rest api development", false));
+
+        List<OnlineClass> javaClasses = new ArrayList<>();
+        javaClasses.add(new OnlineClass(6, "The Java, Test", true));
+        javaClasses.add(new OnlineClass(7, "The Java, Code manipulation", true));
+        javaClasses.add(new OnlineClass(8, "The Java, 8 to 11", true));
+
+        List<List<OnlineClass>> sungbinEvents = new ArrayList<>();
+        sungbinEvents.add(springClasses);
+        sungbinEvents.add(javaClasses);
+
+        System.out.println("spring으로 시작하는 수업");
+        List<OnlineClass> spring = springClasses.stream().filter(s -> s.getTitle().startsWith("spring")).collect(Collectors.toList());
+        spring.forEach(s -> System.out.println(s.getTitle()));
+
+        System.out.println("close 되지 않은 수업");
+//        springClasses.stream().filter(s -> !s.isClosed()).forEach(s -> System.out.println(s.getTitle()));
+        springClasses.stream().filter(Predicate.not(OnlineClass::isClosed)).forEach(s -> System.out.println(s.getTitle()));
+
+        System.out.println("수업 이름만 모아서 스트림 만들기");
+        springClasses.stream().map(OnlineClass::getTitle).forEach(System.out::println);
+
+        System.out.println("두 수업 목록에 들어가있는 모든 수업 아이디 출력");
+//        sungbinEvents.stream().map(s -> s.stream().map(OnlineClass::getId).collect(Collectors.toList())).forEach(System.out::println);
+        sungbinEvents.stream().flatMap(Collection::stream).forEach(s -> System.out.println(s.getId()));
+
+        System.out.println("10부터 1씩 증가하는 무제한 스트림 중에서 앞에 10개 빼고 최대 10개까지만");
+        Stream.iterate(10, i -> i + 1).skip(10).limit(10).forEach(System.out::println);
+
+        System.out.println("자바 수업중에 Test가 들어있는 수업이 있는지 확인");
+//        javaClasses.stream().filter(s -> s.getTitle().contains("Test")).forEach(s -> System.out.println(s.getTitle()));
+        boolean test = javaClasses.stream().anyMatch(s -> s.getTitle().contains("Test"));
+        System.out.println(test);
+
+        System.out.println("스프링 수업 중에 제목에 spring이 들어간 것만 모아서 List로 만들기");
+        List<String> spring1 = springClasses.stream().map(OnlineClass::getTitle).filter(title -> title.contains("spring")).collect(Collectors.toList());
+        spring1.forEach(System.out::println);
     }
 }
