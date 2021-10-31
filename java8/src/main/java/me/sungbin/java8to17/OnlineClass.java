42c8e89cee241abc63520b60221e3d5677f974cd
Robert.Yang <ysb05222@naver.com>
2021-10-31 14:24:09 +0900

    Optional 소개
    Optional 소개

--- java8/src/main/java/me/sungbin/java8to17/OnlineClass.java
+++ java8/src/main/java/me/sungbin/java8to17/OnlineClass.java
@@ -1,5 +1,7 @@
 package me.sungbin.java8to17;
 
+import java.util.Optional;
+
 public class OnlineClass {
 
     private Integer id;
@@ -7,6 +9,8 @@ public class OnlineClass {
     private String title;
 
     private boolean closed;
+
+    public Progress progress;
 
     public OnlineClass(Integer id, String title, boolean closed) {
         this.id = id;
@@ -36,5 +40,15 @@ public class OnlineClass {
 
     public void setClosed(boolean closed) {
         this.closed = closed;
+    }
+
+    // 로직을 처리할때 예외처리를 쓰는것은 좋은 코드가 아니다.
+    // Optional은 리턴 타입에만 쓰는것이 권장사항이다.
+    public Optional<Progress> getProgress() {
+        return Optional.ofNullable(progress);
+    }
+
+    public void setProgress(Progress progress) {
+        this.progress = progress;
     }
 }
